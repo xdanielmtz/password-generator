17285c6571f3e5f231f38d733d11c71334cec75c
Dani JM <xdanielmtz@gmail.com>
2020-09-20 03:04:13 -0400

    added first prompt upon clicking generate password button.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var availableChoices = " "
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,9 +10,20 @@ function writePassword() {
 }
 
 function generatePassword(){
+  var askUser = prompt("How many characters would you like the password to have?"); 
+    i
+  
+  
+  
+  
   return "Generated password here.";
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// prompt the user. how many characters do you want the password to contain. 
+// add a confirm to include special characters.
+// add a confirm to include number characters.
+// add a confirm to add lowercase && uppercase
