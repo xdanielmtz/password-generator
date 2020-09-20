3c5eb6f3b068b62a1eb907da1026ef4a968d2a99
Dani JM <xdanielmtz@gmail.com>
2020-09-20 19:17:35 -0400

    added an object

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var availableChoices = {
+   "letters":[
+     "q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"
+   ],
+   "specialCharacters":[
+    "~","`","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]","|","/",":",";","'","<",">",",",".","?"
+   ],
+   "numbers":[
+     "1","2","3","4","5","6","7","8","9","0"
+   ],
+   "upperCase":[
+    "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"
+   ],
+  }
+
+var randomIndex = math.floor(math.random()*27);
+console.log("Random Index:" randomIndex);
+
   passwordText.value = password;
 }
 
 function generatePassword(){
   var askUser = prompt("How many characters would you like the password to have?"); 
-    i
-  
-  
+  var askSpecialCharecters = confirm("Do you want to include special characters?");
+  var askNumbers = confirm("Do you want to include numbers?");
+  var askUpperCase = confirm("Do you want to include uppercase characters?");
   
   
   return "Generated password here.";
