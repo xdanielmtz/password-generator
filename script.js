cb4cdaf30d910f6fabe03b503a3c41d147ad848b
Dani JM <xdanielmtz@gmail.com>
2020-09-23 17:07:53 -0400

    added the return generatepassword fucntion to two if statments: the min and max statments. also made placeholder variables to store the users choices and pair them up

--- script.js
+++ script.js
@@ -3,37 +3,55 @@ var generateBtn = document.querySelector("#generate");
 var availableChoices = " "
 
 
-//make each object its own array
-var availablelLetters = "qwertyuiopasdfghjklzxcvbnm";
+
+
+//make each object its own variable.
+var availableLetters = "qwertyuiopasdfghjklzxcvbnm";
 var specialCharacters = "~`!@#$%^&*()-_+={}[]|/,:;'<>,.?";
 var availableNumbers = "1234567890";
 var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var thePassword = ""
+ //Create a variable - the variable will be the place holder for the clients user type -
+  // meaning it will store the choices the user makes while iterating through the prompted questions
+var passChoices = ""
+
+
 
  //This function will begin the password creation process 
  function generatePassword(){
-
-  //Create a variable - the variable will be the place holder for the clients user type -
-  // meaning it will store the choices the user makes while iterating through the prompted questions
-  //lookup parsing 
   var passwordType = parseInt(
     prompt("How many characters would you like the password to have?")
   );
   //Begin conditional statement - confirm that the user has typed an actual number and not an integer
   // This statement will determine if the client  typed a number or a interger 
   if (isNaN(passwordType) === true) {
-    var length = alert('Password length must be a number')
+    var length = alert("Password length must be a number");
     return generatePassword();  
   }
   if (passwordType > 128){
     alert("Password should have a max of 128 characters.");
+    return generatePassword();
   }
   if (passwordType < 8){
     alert("Password should have a minimum of 8 characters.");
+    return generatePassword();
   }
-    var askSpecialCharecters = confirm("Do you want to include special characters?");
+    var askSpecialCharacters = confirm("Do you want to include special characters?");
+    if (askSpecialCharacters){
+      passChoices += specialCharacters;
+    }
     var askNumbers = confirm("Do you want to include numbers?");
+    if (askNumbers){
+      passChoices += availableNumbers;
+    }
     var askUpperCase = confirm("Do you want to include uppercase characters?");
-    
+    if (askUpperCase){
+      passChoices += upperCase;
+    }
+    var askLowerCase = confirm("Do you want to include lowercase characters?");
+    if (askLowerCase){
+      passChoices += availableLetters;
+    }
   //Create a conditional statement to assure that the user has - the proper min and max length 
   //this conditional statement will check to assure there are a in on 18 chars
   // if (apple < 18){
@@ -95,8 +113,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// prompt the user. how many characters do you want the password to contain. 
-// add a confirm to include special characters.
-// add a confirm to include number characters.
-// add a confirm to add lowercase && uppercase
